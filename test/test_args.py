caa8cbe189ec2513aa678953c7a1eb39fa4ab206
LucianoFromTrelew <lucianoserruya@hotmail.com>
2018-01-22 18:39:23 -0300

    CLI args parsing tests

---
+++ test/test_args.py
@@ -0,0 +1,28 @@
+import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.main import create_parser
+
+class CLITestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.parser = create_parser()
+
+class TestArgs(CLITestCase):
+    """Test for parsing args"""
+
+    def setUp(self):
+        self.args = ["some_project", "extra_arg"]
+
+    def test_accepts_only_one_arg(self):
+        process = self.parser.parse_args(self.args[:1])
+        self.assertEqual(True, True)
+
+    def test_does_not_accept_more_than_one_arg(self):
+        with self.assertRaises(SystemExit):
+            self.parser.parse_args(self.args)
+
+if __name__ == "__main__":
+    unittest.main()
